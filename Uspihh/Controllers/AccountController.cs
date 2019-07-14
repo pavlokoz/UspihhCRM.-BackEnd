f6e948299085944e68e1c1d8972cc04ddc78d78a
pavlokoz <shostakpavlo@gmail.com>
2019-07-14 21:58:07 +0300

    Add roleId to auth properties

--- Uspihh/Controllers/AccountController.cs
+++ Uspihh/Controllers/AccountController.cs
@@ -265,7 +265,7 @@ namespace Uspihh.Controllers
                 ClaimsIdentity cookieIdentity = await user.GenerateUserIdentityAsync(UserManager,
                     CookieAuthenticationDefaults.AuthenticationType);
 
-                AuthenticationProperties properties = ApplicationOAuthProvider.CreateProperties(user.UserName);
+                AuthenticationProperties properties = ApplicationOAuthProvider.CreateProperties(user);
                 Authentication.SignIn(properties, oAuthIdentity, cookieIdentity);
             }
             else
