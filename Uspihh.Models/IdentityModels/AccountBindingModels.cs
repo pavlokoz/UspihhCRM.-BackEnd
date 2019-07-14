4d1f47fc0af151d7ca7f09fbeea3575b01aab8f5
pavlokoz <shostakpavlo@gmail.com>
2019-07-14 23:15:55 +0300

    Fix build errors

--- Uspihh.Models/IdentityModels/AccountBindingModels.cs
+++ Uspihh.Models/IdentityModels/AccountBindingModels.cs
@@ -53,29 +53,10 @@ namespace Uspihh.Models.IdentityModels
         public string ConfirmPassword { get; set; }
     }
 
-    public class RegisterOfUserBindingModel
+    public class RegisterOfUserBindingModel: RegisterBindingModel
     {
         [Required]
-        public string Email { get; set; }
-
-        [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        [Required]
         public int Role { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
-
-
     }
 
     public class RegisterExternalBindingModel
